2f8d2860b6d0f9bd95178e8b16eb69986ed74403
Kaspars Mickevics <kaspars@fx.lv>
2015-11-08 19:22:52 +0200

    Nicer status output while waiting

--- ssh-when-up.py
+++ ssh-when-up.py
@@ -80,28 +80,52 @@ def main():
     q = Queue()
     sleepy = Sleepy()
     print "Connecting to {}".format(target)
+    down_time_begin = datetime.datetime.now()
 
     t = Thread(target=sleep_till_host_responds,args=(target,q))
     t.start()
-
+    # use dots to indicete that stuff is happening
     dot = "."
+    dot_length = 1
+    increment_dot = True
+    dot_max_length = 20
+    progress_container = "[ {} ]"
+    cursor = "->"
     while t.isAlive():
-        msg = "{}: ".format(datetime.datetime.now())
+        timestamp = datetime.datetime.now()
+        timestamp = timestamp.strftime("%d.%m.%y %H:%M:%S")
+        msg = "{}: ".format(timestamp)
+        # if the host refuses connection, we start adding stuff to queue
+        # not ideal but this is how we communicate that host is actually up
         if q.qsize() > 0:
             msg += "The host is not accepting SSH"
         else:
             msg += "Waiting for the host to come up"
-        msg += "{}\r".format(dot)
+        msg += " "
+        if dot_length < dot_max_length and increment_dot:
+            dot_length += 1
+        elif dot_length == dot_max_length and increment_dot:
+            increment_dot = False
+            cursor = "<-"
+        elif not increment_dot:
+            dot_length = dot_length -1
+            if dot_length == 1:
+                increment_dot = True
+                cursor = "->"
+        right_padding = dot_max_length - dot_length
+        progress = "{}{}{}".format(dot*dot_length, cursor, "."*right_padding)
+        msg += progress_container.format(progress)
+        msg += "\r"
         sys.stdout.write(msg)
         sys.stdout.flush()
         time.sleep(0.1)
-        dot += "."
-        if len(dot)> 60:
-            dot="."
-            print 
+
     else:
+        down_time_end = datetime.datetime.now()
+        down_time = down_time_end - down_time_begin
         print
         print "Host is up, connecting."
+        print "Down time: {}".format(down_time)
         start_time = datetime.datetime.now()
         call("ssh {}".format(target), shell=True)
         end_time = datetime.datetime.now()
