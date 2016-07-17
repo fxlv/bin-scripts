98f876c81ccddea2e43b99758a5037ea13f240c9
Kaspars Mickevics <kaspars@fx.lv>
2016-07-17 10:34:09 +0300

    Double quoting variables

--- ilossh.sh
+++ ilossh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "Please provide ILO hostname/IP as the first argument"
     exit 1
 fi
-ssh -o "PreferredAuthentications password" -oKexAlgorithms=diffie-hellman-group1-sha1   Administrator@${1}
+ssh -o "PreferredAuthentications password" -oKexAlgorithms=diffie-hellman-group1-sha1   Administrator@"${1}"
